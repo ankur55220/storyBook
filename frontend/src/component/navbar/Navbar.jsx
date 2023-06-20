0a5d6e5668cc28b6de51ba97802215a36f670cb3
Ankur <ankur55420@gmail.com>
2023-06-20 21:25:12 +0530

    navbar logo name changed

--- frontend/src/component/navbar/Navbar.jsx
+++ frontend/src/component/navbar/Navbar.jsx
@@ -99,7 +99,7 @@ function Navbar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            StoryBook
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
