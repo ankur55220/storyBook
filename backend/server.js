88b51be9d657059d604988a08be9071c0079f987
Ankur verma <38664111+ankur55220@users.noreply.github.com>
2023-08-22 13:50:31 +0530

    Update server.js
    same bracket

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import app from "./app.js";
 import DataConn from './config/database.js';
 const port= process.env.PORT || 4000
-DataConn().then(()=>{
-    app.listen(port,()=>{
 
-    console.log(`listening at port 4000 ${port}`)
+
+DataConn.then(()=>{
+
+    app.listen(port,()=>{
+         console.log(`listening at port 4000 ${port}`)
     })
+    
 })
+
     
 
 
