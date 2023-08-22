9a9197fef954451999068d01975b573a70cc3f74
Ankur verma <38664111+ankur55220@users.noreply.github.com>
2023-08-22 13:37:00 +0530

    Update server.js
    put listen under mongodb connection callback

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import app from "./app.js";
 import DataConn from './config/database.js';
-
-DataConn()
 const port= process.env.PORT || 4000
-app.listen(port,()=>{
+DataConn().then(()=>{
+    app.listen(port,()=>{
 
-    console.log("listening at port 4000")
+    console.log(`listening at port 4000 ${port}`)
 })
+    )}
+
+
