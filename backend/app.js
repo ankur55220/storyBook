dfd09b5f3ae0bd23184ef9043407e0fbd8afa71f
Ankur verma <38664111+ankur55220@users.noreply.github.com>
2023-08-22 13:12:53 +0530

    Update app.js
    removed import DataConn from './config/database.js';

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
 import { fileURLToPath } from 'url';
-import DataConn from './config/database.js';
+
 import auth from './routes/Auth.js'
 import StoryRoutes from './routes/StoryRoutes.js'
 import AudioRoutes from './routes/AudioRoutes.js'
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({path:path.join(__dirname,"/config/config.env")})
 
-DataConn();
+
 
 
 
